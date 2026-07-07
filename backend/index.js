2e34977c817fa9ecbd1296bca1b23401325a82fe
AhmedSaied1212 <www.ahmedsaied1212@gmail.com>
2026-07-07 15:28:56 +0300

    adding .env.example

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,8 @@ const app = express();
 
 connectDB();
 
-const allowedOrigins = ["http://localhost:5173", process.env.FRONTEND_URL];
+const allowedOrigins = ["http://localhost:5173"];
+const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
@@ -29,33 +30,12 @@ app.use(
   })
 );
 
-// 1. Configure Cloudinary
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
-
-// 2. Configure Multer Memory Storage
-const storage = multer.memoryStorage();
-const upload = multer({ 
-  storage: storage,
-  limits: { fileSize: 2 * 1024 * 1024 }, // Limit file size to 2MB
-  fileFilter: (req, file, cb) => {
-    // Accept images only
-    if (file.mimetype.startsWith('image/')) {
-      cb(null, true);
-    } else {
-      cb(new Error('Only image files are allowed!'), false);
-    }
-  }
-});
-
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/expense", expenseRoutes);
 
-
-module.exports = app;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
